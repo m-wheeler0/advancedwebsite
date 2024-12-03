d251af7c0a36bfd2b1fca610cf47edd8b9519cdc
m-wheeler0 <146672950+m-wheeler0@users.noreply.github.com>
2024-12-03 16:47:36 +0000

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-WTF_CSRF_ENABLED = True
-SECRET_KEY = '8xtZVbzbfooEcvae'
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, 'app.db')
-SQLALCHEMY_TRACK_MODIFICATIONS = True
