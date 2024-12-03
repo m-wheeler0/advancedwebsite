638ece73aa3c0781c968b876236d079e21abda2f
m-wheeler0 <146672950+m-wheeler0@users.noreply.github.com>
2024-12-03 16:46:22 +0000

    Add files via upload

--- app/login_form.py
+++ app/login_form.py
@@ -1,14 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, TextField
+from wtforms import PasswordField, StringField
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import User
 
 class LoginForm(FlaskForm):
-    username = TextField('Username: ', validators=[DataRequired()])
+    username = StringField('Username: ', validators=[DataRequired()])
     password = PasswordField('Password: ', validators=[DataRequired()])
 
 class RegisterForm(FlaskForm):
-    username = TextField('Username: ', validators=[DataRequired()])
+    username = StringField('Username: ', validators=[DataRequired()])
     password = PasswordField('Password: ', validators=[DataRequired()])
 
     def validate_username(self, field):
